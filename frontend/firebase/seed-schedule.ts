eac45957b3687feb1784a67f41d25e9374ebbf77
Jahanzaib Raza <jraza198@gmail.com>
2026-06-18 20:44:18 +0500

    removed extra noise from .env and firebase setup

--- frontend/firebase/seed-schedule.ts
+++ frontend/firebase/seed-schedule.ts
@@ -40,7 +40,6 @@ async function main() {
     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
     authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
     projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
     appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
   });
 
